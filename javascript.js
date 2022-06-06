b556e4bbde2c8b7ee187c86e7d6d6cfb7e0c95c1
ijacobsen <ian.jacobsen.92@gmail.com>
2022-06-06 16:43:46 -0400

    added javascript to parse order form and draft email

--- javascript.js
+++ javascript.js
@@ -3,7 +3,18 @@ var form = document.getElementById('request-form');
 form.addEventListener("submit", function(e) {
     e.preventDefault();
     const data = new FormData(form);
-    for (const [name,value] of data) {
-      console.log(name, ":", value)
-    }
+
+    let name = data.get('name');
+    let dough = data.get('dough');
+    let date = data.get('date');
+    let topping = data.getAll('topping');
+
+    console.log('-------send email-------')
+
+    let toppings = topping.reduce((ts, t) => ts + ', ' + t);
+    let msg = name + ' would like to have a pizzer with ';
+    msg += toppings;
+    msg += ' on ' + dough + ' dough';
+    msg += ' on ' + date;
+    console.log(msg);
   })
